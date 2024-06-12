34611ae427b67bccdd8a13983258b99c21a3fa18
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-06-12 17:29:38 -0300

    Entidades criadas

---
+++ src/entities/Pessoa.java
@@ -0,0 +1,42 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Pessoa() {
+
+    }
+
+    public Pessoa(String nome, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa [nome=" + nome + ", dataNascimento=" + dataNascimento + "]";
+    }
+
+    
+    
+}
